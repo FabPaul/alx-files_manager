78d8595130eed6cc8720909760806315b349c3ef
FabPaul <paulfopa0@gmail.com>
2024-03-10 12:50:27 +0100

    fixed: eslint and ndusers and ndfiles function

--- utils/db.js
+++ utils/db.js
@@ -1,31 +1,32 @@
 const { MongoClient } = require('mongodb');
 
-class DBClient{
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
-        const uri = `mongodb://${host}:${port}/${database}`;
-        this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    }
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  }
 
-    isAlive() {
-        if (this.client.connected) {
-            return true;
-        } else {
-            return false;
-        }
+  isAlive() {
+    if (this.client.connected) {
+      return true;
     }
+    return false;
+  }
 
-    async nbUsers() {
-        const collection = this.client.db().collection('users');
-        return collection.countDocuments();
-    }
+  async nbUsers() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('users').countDocuments();
+    return users;
+  }
 
-    async nbFiles() {
-        const collection = this.client.db().collection('files');
-        return collection.countDocuments();
-    }
+  async nbFiles() {
+    await this.client.connect();
+    const users = await this.client.db(this.database).collection('files').countDocuments();
+    return users;
+  }
 }
 
 const dbClient = new DBClient();
